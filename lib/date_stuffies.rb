b49111b93e6e187fb7a266fd7f61eed32d87621e
sasha mussich <alexandra.muisch@gmail.com>
2016-07-21 11:47:04 -0300

    working so far very well

--- lib/date_stuffies.rb
+++ lib/date_stuffies.rb
@@ -1,6 +1,6 @@
 module DateStuffies
 
-	Time::DATE_FORMATS[:just_date]  = ->(time) { time.strftime("%B #{time.day.ordinalize}") }
+	Time::DATE_FORMATS[:just_date]  = ->(time) { time.strftime("%B #{time.day.ordinalize} %Y") }
 
 	def self.pretty_just_date_format date
 		date.to_formatted_s(:just_date)

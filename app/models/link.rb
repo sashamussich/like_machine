9fbfadc9b22c1bd1e6f6357bb8ebd8a695287d9f
sasha mussich <alexandra.muisch@gmail.com>
2016-07-21 17:50:17 -0300

    js improved

--- app/models/link.rb
+++ app/models/link.rb
@@ -8,7 +8,7 @@ class Link < ActiveRecord::Base
    default_scope { order('cast(created_at as date) desc, cached_votes_up desc') } 
 
     def self.make_hash_by_date links
-		hash = links.group_by{|link| DateStuffies.pretty_just_date_format link.created_at}
+		  hash = links.group_by{|link| DateStuffies.pretty_just_date_format link.created_at}
     	# self.sort_by_highest_votes_score hash
       # hash.each do |date, links|
       #   links.to_a.sort_by{|link| link[:cashed_votes_score]}

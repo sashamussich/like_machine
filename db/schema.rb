33f6736d0464cbeea813209ef03337c98ea48185
sasha mussich <alexandra.muisch@gmail.com>
2016-07-28 07:24:22 -0300

    gemfile altered - postgres

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160721144847) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "links", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
@@ -26,16 +28,15 @@ ActiveRecord::Schema.define(version: 20160721144847) do
     t.integer  "cached_weighted_score",   default: 0
     t.integer  "cached_weighted_total",   default: 0
     t.float    "cached_weighted_average", default: 0.0
+    t.index ["cached_votes_down"], name: "index_links_on_cached_votes_down", using: :btree
+    t.index ["cached_votes_score"], name: "index_links_on_cached_votes_score", using: :btree
+    t.index ["cached_votes_total"], name: "index_links_on_cached_votes_total", using: :btree
+    t.index ["cached_votes_up"], name: "index_links_on_cached_votes_up", using: :btree
+    t.index ["cached_weighted_average"], name: "index_links_on_cached_weighted_average", using: :btree
+    t.index ["cached_weighted_score"], name: "index_links_on_cached_weighted_score", using: :btree
+    t.index ["cached_weighted_total"], name: "index_links_on_cached_weighted_total", using: :btree
+    t.index ["user_id"], name: "index_links_on_user_id", using: :btree
   end
-
-  add_index "links", ["cached_votes_down"], name: "index_links_on_cached_votes_down"
-  add_index "links", ["cached_votes_score"], name: "index_links_on_cached_votes_score"
-  add_index "links", ["cached_votes_total"], name: "index_links_on_cached_votes_total"
-  add_index "links", ["cached_votes_up"], name: "index_links_on_cached_votes_up"
-  add_index "links", ["cached_weighted_average"], name: "index_links_on_cached_weighted_average"
-  add_index "links", ["cached_weighted_score"], name: "index_links_on_cached_weighted_score"
-  add_index "links", ["cached_weighted_total"], name: "index_links_on_cached_weighted_total"
-  add_index "links", ["user_id"], name: "index_links_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string "provider"
@@ -44,18 +45,17 @@ ActiveRecord::Schema.define(version: 20160721144847) do
   end
 
   create_table "votes", force: :cascade do |t|
-    t.integer  "votable_id"
     t.string   "votable_type"
-    t.integer  "voter_id"
+    t.integer  "votable_id"
     t.string   "voter_type"
+    t.integer  "voter_id"
     t.boolean  "vote_flag"
     t.string   "vote_scope"
     t.integer  "vote_weight"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope", using: :btree
+    t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
   end
-
-  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
-  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
 
 end

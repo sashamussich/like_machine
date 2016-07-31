5bc0b4fcebb257892c8177695552540195361c14
sasha mussich <alexandra.muisch@gmail.com>
2016-07-31 17:35:44 -0300

    websockets working now. just current_user to be added to ActionJob

--- app/jobs/links_updates_broadcast_job.rb
+++ app/jobs/links_updates_broadcast_job.rb
@@ -2,12 +2,12 @@ class LinksUpdatesBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform link
-    ActionCable.server.broadcast "links_channel", { link: render_link_voting_buttons(link), link_id: link.id}
+    ActionCable.server.broadcast "links_channel", { link_partial: render_link(link), link_id: link.id } 
   end
 
   private
 
-  def render_link_voting_buttons link
-  	LinksController.render :_link, locals: { link: link }
+  def render_link link
+  	ApplicationController.renderer.render(partial: 'links/link', locals: { link: link, current_user: User.first })
   end
 end
